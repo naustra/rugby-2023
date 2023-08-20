3fcbb570d919a88fffbe03522a22271a6b773a42
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-08-20 17:08:00 +0200

    add tailwind

--- src/theme.js
+++ src/theme.js
@@ -1,34 +1,32 @@
 import teal from '@mui/material/colors/teal'
 import cyan from '@mui/material/colors/cyan'
-import { createTheme, adaptV4Theme } from '@mui/material/styles'
+import { createTheme } from '@mui/material/styles'
 
-const theme = createTheme(
-  adaptV4Theme({
-    palette: {
-      primary: cyan,
-      secondary: teal,
+const theme = createTheme({
+  palette: {
+    primary: cyan,
+    secondary: teal,
+  },
+  typography: {
+    h1: {
+      fontSize: '1.5rem',
     },
-    typography: {
-      h1: {
-        fontSize: '1.5rem',
-      },
-      h2: {
-        fontSize: '1.25rem',
-      },
-      h3: {
-        fontSize: '1rem',
-      },
-      h4: {
-        fontSize: '0.75rem',
-      },
-      h5: {
-        fontSize: '0.50rem',
-      },
-      h6: {
-        fontSize: '0.25rem',
-      },
+    h2: {
+      fontSize: '1.25rem',
     },
-  }),
-)
+    h3: {
+      fontSize: '1rem',
+    },
+    h4: {
+      fontSize: '0.75rem',
+    },
+    h5: {
+      fontSize: '0.50rem',
+    },
+    h6: {
+      fontSize: '0.25rem',
+    },
+  },
+})
 
 export default theme
